e05f7f272dcb4fe4a1e84c49b051aebea9d8d2e5
Ptalat2 <ptalat2@uic.edu>
2024-02-09 17:28:24 -0600

    script added js

---
+++ script.js
@@ -0,0 +1,13 @@
+ // Get main elements
+ const main = document.querySelector('main');
+ const form = document.querySelector('form')
+ const info = document.querySelector('.info');
+ const reportBtn = document.getElementById('report');
+ const footer = document.querySelector("footer");
+ const header = document.querySelector("header");
+ const search = document.querySelector("#search");
+ const mapView = document.querySelector(".map-ct");
+ const home = document.querySelector("#home");
+ const bookmarksBtn = document.getElementById('bookmarksBtn');
+ const bookmarksForm = document.querySelector('.bookmark');
+ const snackbar = new mdc.snackbar.MDCSnackbar(document.querySelector('.mdc-snackbar'));
